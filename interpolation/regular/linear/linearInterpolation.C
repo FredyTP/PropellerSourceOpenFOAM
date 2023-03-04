f2319f37746de4654950894791280b881f1e8be3
FredyTP7 <altorpon@upv.es>
2023-03-04 17:19:04 +0100

    Refactor and 2d linear interpolation

--- interpolation/regular/linear/linearInterpolation.C
+++ interpolation/regular/linear/linearInterpolation.C
@@ -5,7 +5,7 @@ namespace Foam
 
 
 
-linearInterpolation1::linearInterpolation1
+linearInterpolation1D::linearInterpolation1D
 (
     const List<scalar> inputs,
     const List<scalar> outputs
@@ -19,7 +19,7 @@ outputs_(outputs)
     }
 }
 
-scalar linearInterpolation1::interpolate(FixedList<scalar, 1> input){
+scalar linearInterpolation1D::interpolate(FixedList<scalar, 1> input){
     scalar x = input[0];
 
     label i1,i2;
@@ -47,49 +47,11 @@ scalar linearInterpolation1::interpolate(FixedList<scalar, 1> input){
 
 }
 
-label linearInterpolation1::findIndex(scalar input,label& i1, label& i2) const
+label linearInterpolation1D::findIndex(scalar input,label& i1, label& i2) const
 {
-    if(input<inputs_[0])
-    {
-        //indexes are both set to closest index
-        i1=0;
-        i2=0;
-        //out of bounds
-        return 0;
-    }
-    for(int i =0; i< inputs_.size()-1; i++)
-    {
-        //If (luckily) there is coincidence
-        if(input == inputs_[i])
-        {
-            i1=i;
-            i2=i;
-            return 1;
-        }
-
-        //Between i and i+1
-        if(inputs_[i] < input && input< inputs_[i+1])
-        {
-            i1=i;
-            i2=i+1;
-            return 1;
-        }
-    }
-
-    if(input==inputs_[inputs_.size()-1])
-    {
-        i1=inputs_.size()-1;
-        i2=i1;
-        return 1;
-    }
-
-    //Outside upper bounds
-    i1=inputs_.size()-1;
-    i2=i1;
-    return 0;
-
+    return linearInterpolation1D::FindIndex(input,inputs_,i1,i2);
 }
-void linearInterpolation1::interpolationCoefficients(scalar input, label i1, label i2, scalar &a1, scalar &a2) const
+void linearInterpolation1D::interpolationCoefficients(scalar input, label i1, label i2, scalar &a1, scalar &a2) const
 {
     scalar x,x1,x2;
 
@@ -99,6 +61,60 @@ void linearInterpolation1::interpolationCoefficients(scalar input, label i1, lab
 
     a1 = (x2-x)/(x2-x1);
     a2 = (x-x1)/(x2-x1);
-}   
+}
+
+void linearInterpolation1D::InterpolationCoefficients(scalar input, const List<scalar> &inputs, label i1, label i2, scalar &a1, scalar &a2)
+{
+    scalar x,x1,x2;
+
+    x = input;
+    x1 = inputs[i1];
+    x2 = inputs[i2];
+
+    a1 = (x2-x)/(x2-x1);
+    a2 = (x-x1)/(x2-x1);
+}
+
+label linearInterpolation1D::FindIndex(scalar input, const List<scalar> &inputs, label &i1, label &i2)
+{
+    if(input<inputs[0])
+    {
+        //indexes are both set to closest index
+        i1=0;
+        i2=0;
+        //out of bounds
+        return 0;
+    }
+    for(int i = 0; i< inputs.size()-1; i++)
+    {
+        //If (luckily) there is coincidence
+        if(input == inputs[i])
+        {
+            i1=i;
+            i2=i;
+            return 1;
+        }
+
+        //Between i and i+1
+        if(inputs[i] < input && input< inputs[i+1])
+        {
+            i1=i;
+            i2=i+1;
+            return 1;
+        }
+    }
+
+    if(input==inputs[inputs.size()-1])
+    {
+        i1=inputs.size()-1;
+        i2=i1;
+        return 1;
+    }
+
+    //Outside upper bounds
+    i1=inputs.size()-1;
+    i2=i1;
+    return 0;
+}
 
 }
--- interpolation/regular/regularInterpolation.C
+++ interpolation/regular/regularInterpolation.C
@@ -1,11 +1,2 @@
 #include "regularInterpolation.H"
 
-
-
-
-template<unsigned int dimension>
-Foam::regularInterpolation<dimension>::regularInterpolation()
-{
-
-}
-
