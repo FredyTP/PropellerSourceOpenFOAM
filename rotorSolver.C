1cdeef4b97cc9ada79c7943f575a27d4ebee0be0
FredyTP7 <altorpon@upv.es>
2023-03-06 17:55:34 +0100

    Moved repo to include Make config and VSCode config

---
+++ rotorSolver.C
@@ -0,0 +1,162 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    pisoFoamerror
+
+Group
+    grpIncompressibleSolvers
+
+Description
+    Transient solver for incompressible, turbulent flow, using the PISO
+    algorithm.
+
+    \heading Solver details
+    The solver uses the PISO algorithm to solve the continuity equation:
+
+        \f[
+            \div \vec{U} = 0
+        \f]
+
+    and momentum equation:
+
+        \f[
+            \ddt{\vec{U}} + \div \left( \vec{U} \vec{U} \right) - \div \gvec{R}
+          = - \grad p
+        \f]
+
+    Where:
+    \vartable
+        \vec{U} | Velocity
+        p       | Pressure
+        \vec{R} | Stress tensor
+    \endvartable
+
+    Sub-models include:
+    - turbulence modelling, i.e. laminar, RAS or LES
+    - run-time selectable MRF and finite volume options, e.g. explicit porosity
+
+    \heading Required fields
+    \plaintable
+        U       | Velocity [m/s]
+        p       | Kinematic pressure, p/rho [m2/s2]
+        \<turbulence fields\> | As required by user selection
+    \endplaintable
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulentTransportModel.H"
+#include "pisoControl.H"
+#include "fvOptions.H"
+#include "linearInterpolation.H"
+#include "interpolated.H"
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+
+    interpolated<scalar,scalar,scalar> it;
+    
+    linearInterpolation<scalar,scalar,3> lin({{1,2},{1,2},{1,2}},{0,1,2,3,4,5,6,7},{2,1,0});
+    /**
+     *  (1 1) -> 1
+     *  (2 1) -> 2
+     *  (1 2) -> 3
+     *  (2 2) -> 4
+    */  
+
+    Info<<lin<<endl;
+    List<scalar> out;
+    List<scalar> coeff;
+
+    lin.interpolationList({1.1,1.1,1.1},coeff,out);
+    it.coefficients()=coeff;
+    it.points()=out;
+
+    Info<<"Output nodes: "<<out<<endl;
+    Info<<"Output coeff:" <<coeff<<endl;
+
+
+    Info<<"Interpolated: "<<it.interpolate()<<endl;
+
+    return 0;
+    argList::addNote
+    (
+        "Transient solver for incompressible, turbulent flow,"
+        " using the PISO algorithm."
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createFields.H"
+    #include "initContinuityErrs.H"
+
+    turbulence->validate();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include "CourantNo.H"
+
+        // Pressure-velocity PISO corrector
+        {
+            #include "UEqn.H"
+
+            // --- PISO loop
+            while (piso.correct())
+            {
+                #include "pEqn.H"
+            }
+        }
+
+        laminarTransport.correct();
+        turbulence->correct();
+        
+        runTime.write();
+
+        runTime.printExecutionTime(Info);
+
+        
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
