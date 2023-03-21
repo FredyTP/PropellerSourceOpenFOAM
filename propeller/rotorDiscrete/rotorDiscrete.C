ad6b21147c8081b38e391d9455aa233a6478b5a0
FredyTP7 <altorpon@upv.es>
2023-03-21 18:34:44 +0100

    example using voronoid

--- propeller/rotorDiscrete/rotorDiscrete.C
+++ propeller/rotorDiscrete/rotorDiscrete.C
@@ -4,6 +4,10 @@
 #include "geometricOneField.H"
 #include "unitConversion.H"
 #include "rotorGeometry.H"
+#include "delaunayTriangulation.H"
+#include <fstream>
+
+
 namespace Foam
 {
 
@@ -101,6 +105,139 @@ void rotorDiscrete::fromRotorMesh(const rotorMesh &rotorMesh)
     }
 
     volume.write();
+
+
+
+    //-------------BEGIN JUST TEST-------------//
+
+        /*List<XYZ> ps(6);
+        ps[0] = XYZ{-0.1,-0.1,0};
+        ps[1] = XYZ{0.1,-0.1,0};
+        ps[2] = XYZ{-0.1,0.1,0};
+        ps[3] = XYZ{0.1,0.1,0};
+        ps[4] = XYZ{0,0,0};
+        ps[5] = XYZ{0.15,0,0};*/
+        List<point> ps(cylPoints_.size());
+        for(label i = 0; i <cylPoints_.size(); i++)
+        {
+            point pt = cylCS_.globalPosition(cylPoints_[i]);
+            ps[i] = point{pt.x(),pt.z(),0};
+        }
+
+        std::sort(ps.begin(),ps.end(),[](const point& p1, const point& p2)
+            {
+                return p1.x() < p2.x();
+            });
+
+        List<point> orto;
+        List<List<label>> voronoid;
+        delaunayTriangulation::Voronoid
+        (
+            ps,
+            orto,
+            voronoid,
+            delaunayTriangulation::circularRegion(0.15),
+            delaunayTriangulation::intersectCircle(0.15)
+        );
+        
+        std::string x_string = "x = [";
+        std::string y_string = "y = [";
+        std::string xc_string = "xc = [";
+        std::string yc_string = "yc = [";
+
+        for(label i = 0 ; i < ps.size();i++)
+        {
+            xc_string += std::to_string(ps[i].x());
+            
+            yc_string += std::to_string(ps[i].y());
+
+            if(i != ps.size()-1)
+            {
+                xc_string +=",";
+                yc_string +=",";
+            }
+        }
+
+        xc_string +="]";
+        yc_string +="]";
+
+        for(label i = 0 ; i < orto.size();i++)
+        {
+            x_string += std::to_string(orto[i].x());
+            
+            y_string += std::to_string(orto[i].y());
+
+            if(i != orto.size()-1)
+            {
+                x_string +=",";
+                y_string +=",";
+            }
+        }
+
+        x_string +="]";
+        y_string +="]";
+
+        std::string tri_str = "tri = [";
+        for(label i = 0; i< voronoid.size(); i++)
+        {
+            auto vor = voronoid[i];
+            if(vor.size()==0) continue;
+            tri_str += "[";   
+            for(label j=0; j <vor.size();j++)
+            {
+                tri_str += std::to_string(vor[j]);
+                tri_str += ",";
+            }
+            tri_str += std::to_string(vor[0]);
+            tri_str += "]";
+            if(i!= voronoid.size()-1)
+            {
+                tri_str += ",";
+            }
+
+        }
+        tri_str += "]";
+
+    std::string pyplot= "import numpy as np;\n";
+    pyplot+= "import matplotlib.pyplot as plot;\n";
+    pyplot += "import matplotlib as mp;\n";
+    pyplot+= xc_string + "\n";
+    pyplot+= yc_string + "\n";
+    pyplot+= x_string + "\n";
+    pyplot+= y_string +"\n";
+    pyplot+= tri_str + "\n";
+
+    pyplot+="tris = []\n";
+    pyplot+="for i in range(len(tri)):\n";
+    pyplot+="\txp = np.zeros(len(tri[i]))\n";
+    pyplot+="\typ = np.zeros(len(tri[i]))\n";
+    pyplot+="\tfor j in range(len(tri[i])):\n";
+    pyplot+="\t\txp[j]=x[tri[i][j]]\n";
+    pyplot+="\t\typ[j]=y[tri[i][j]]\n";
+    //pyplot+="\tif(len(tri[i])>2):\n";
+    pyplot+="\tplot.plot(xp,yp)\n";
+    pyplot+="\n";
+    pyplot+="plot.plot(xc,yc,marker='o',linewidth = 0)\n";
+    pyplot+="plot.show()\n";
+   std::ofstream file("triangulation.py",std::ios::out);
+    file<<pyplot;
+    file.close();
+    /*wchar_t *program = Py_DecodeLocale("./", NULL);
+    if (program == NULL) {
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        //exit(1);
+    }
+    Py_SetProgramName(program); */ /* optional but recommended */
+    /*Py_Initialize();
+    PyRun_SimpleString(pyplot.c_str());
+    if (Py_FinalizeEx() < 0) {
+        //exit(120);
+    }
+    PyMem_RawFree(program);*/
+    
+
+
+     //-------------END JUST TEST-------------//
 }
 bool rotorDiscrete::read(const dictionary &dict)
 {
