5ca477cfa3262e5ebd8f43873267f5d2f535d466
FredyTP7 <altorpon@upv.es>
2023-05-17 21:59:15 +0200

    implemented direct control

--- propeller/rotorControl/bemControl/bemDirectControl.C
+++ propeller/rotorControl/bemControl/bemDirectControl.C
@@ -5,10 +5,57 @@ namespace Foam
 {
  
 defineTypeNameAndDebug(bemDirectControl,0);
-addToRunTimeSelectionTable(base_class,bemDirectControl, dictionary);
+addToRunTimeSelectionTable(rotorControl,bemDirectControl, dictionary);
 
 
-bemDirectControl::bemDirectControl(const dictionary& dict);
+bemDirectControl::bemDirectControl(const dictionary& dict)
+ : bemControl(dict)
+{
+    collectivePitch = dict.getOrDefault<scalar>("collectivePitch",0);
+    sinPitch = dict.getOrDefault<scalar>("sinPitch",0);
+    cosPitch = dict.getOrDefault<scalar>("cosPitch",0);
+    sinAzimuth = dict.getOrDefault<scalar>("sinAzimuth",0);
+    cosAzimuth = dict.getOrDefault<scalar>("cosAzimuth",0);
+    flapping = dict.getOrDefault<scalar>("flapping",0);
+    sinFlapping = dict.getOrDefault<scalar>("sinFlapping",0);
+    cosFlapping = dict.getOrDefault<scalar>("cosFlapping",0);
+}
 
+scalar bemDirectControl::getAzimuth(scalar azimuth0) const
+{
+    return azimuth0-sinAzimuth*sin(azimuth0)-cosAzimuth*cos(azimuth0);
+}
 
+scalar bemDirectControl::getPitch(scalar azimuth) const
+{
+    return collectivePitch-sinPitch*sin(azimuth)-cosPitch*cos(azimuth);
+}
+
+scalar bemDirectControl::getFlapping(scalar azimuth) const
+{
+    return flapping-sinFlapping*sin(azimuth)-cosFlapping*cos(azimuth);
+}
+
+scalar bemDirectControl::getAzimuthDot(scalar azimuth, scalar angularVelocity)
+{
+    return angularVelocity*(1 - sinAzimuth*cos(azimuth) + cosAzimuth*sin(azimuth));
+}
+scalar bemDirectControl::getPitchDot(scalar azimuth, scalar angularVelocity)
+{
+    return angularVelocity*(-sinPitch*cos(azimuth) + cosPitch*sin(azimuth));
+}
+scalar bemDirectControl::getFlappingDot(scalar azimuth, scalar angularVelocity)
+{
+    return angularVelocity*(-sinFlapping*cos(azimuth) + cosFlapping*sin(azimuth));
+}
+
+vector bemDirectControl::getBladeLocalVel(scalar azimuth, scalar angularVelocity, scalar radius)
+{
+    return vector
+    (
+        -getAzimuthDot(azimuth,angularVelocity)*radius, //x-axis "backwards"
+        0,
+        getFlappingDot(azimuth,angularVelocity)*radius  //z-axis "up"
+    );
+}
 }
